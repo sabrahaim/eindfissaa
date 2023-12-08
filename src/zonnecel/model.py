20540d192202edd066b3c57d58006addb2ae990f
sabrahaim <sabrahaim@gmail.com>
2023-12-08 16:43:44 +0100

    IU graph and UUgraph

--- src/zonnecel/model.py
+++ src/zonnecel/model.py
@@ -5,19 +5,34 @@ from controller import ArduinoZonnecel, list_devices
 
 class ExperimentZonnecel:
     def __init__(self, port):
-        rm = pyvisa.RecourceManager("@py")
-        self.device = ArduinoZonnecel(port = port)
+        rm = pyvisa.ResourceManager("@py")
+        self.device = ArduinoZonnecel(port)
     
     def IU(self, start, stop, repeats):
+        voltages_U0 = []
+        voltages_U2 = []
+        voltages_PV = []
+        currents_PV = []
         for value_volt in np.arange(start, stop, 3.3/1023):
-            voltages_U1 = []
-            voltages_U2 = []
             for value in range(repeats):
                 self.device.set_output_voltage(value=value_volt)
+                
+                voltages_U0.append(self.device.get_output_value())
+
                 voltage_U1 = self.device.get_input_voltage(channel=1)
-                voltages_U1.append(voltage_U1)
+                # voltages_U1.append(voltage_U1)
+
                 voltage_U2 = self.device.get_input_voltage(channel=2)
                 voltages_U2.append(voltage_U2)
 
+                voltage_PV = 3 * voltage_U1
+                voltages_PV.append(voltage_PV)
+
+                current_1 = voltage_U1/(3*10**6)
+                current_2 = voltage_U2/4.7
+                current_PV = current_2 - current_1
+                currents_PV.append(current_PV)
+
+
         self.device.set_output_value(value=0)
-        return voltages_U1, voltages_U2
+        return voltages_U0, voltages_PV, currents_PV
--- src/zonnecel/view.py
+++ src/zonnecel/view.py
@@ -1,8 +1,25 @@
+import matplotlib.pyplot as plt
 import pyvisa
-import matplotlib.pyplot as plt 
-
 from model import ExperimentZonnecel
 
+
 def graph():
-    U0_U1_karakteristiek = ExperimentZonnecel()
-    
+    U0_U1_karakteristiek = ExperimentZonnecel("ASRL3::INSTR")
+
+    voltages_U0, voltages_PV, currents_PV = U0_U1_karakteristiek.IU(0, 3.3, 1)
+    print(voltages_U0)
+    print(voltages_PV)
+
+    # plt.plot(voltages_U0, voltages_PV, 'o')
+    # plt.xlabel("Spanning voor bepalen weerstand")
+    # plt.ylabel("Spanning door zonnepaneel")
+    # plt.show()
+
+    plt.plot(voltages_PV, currents_PV, 'o')
+    plt.xlabel("Spanning PV")
+    plt.ylabel("Stroomsterkte PV")
+    plt.show()
+
+
+if __name__ == "__main__":
+    view = graph()
